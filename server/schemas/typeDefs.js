47a8caccc6305df4ae8d933324f1532111c16805
sacylkowski <sacylkowski@gmail.com>
2022-06-20 19:38:42 -0500

    added auth utils

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,6 +31,14 @@ const typeDefs = gql`
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+    type Auth {
+        token: ID!
+        user: User
     }`;
 
 // export the typeDefs
