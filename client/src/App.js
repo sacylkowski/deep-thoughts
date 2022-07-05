d3116b5e77902127f5b55eb393e0fcd96476e9d3
sacylkowski <sacylkowski@gmail.com>
2022-07-05 16:43:21 -0500

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 // ApolloProvider - special type of React component that we'll use to provide data to all of the other components 
 // ApolloClient - a constructor function that will help initialize the connection to the GraphQL API server
@@ -23,13 +29,40 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-        <Home />
+        <Routes>
+          <Route
+          path="/"
+          element={<Home />}
+          />
+          <Route
+          path="/login"
+          element={<Login />}
+          />
+          <Route
+          path="/signup"
+          element={<Signup />}
+          />
+           <Route
+          path="/profile"
+          element={<Profile />}
+          />
+           <Route
+          path="/thought"
+          element={<SingleThought />}
+          />
+           <Route
+          path="*"
+          element={<NoMatch />}
+          />
+        </Routes>
       </div>
       <Footer />
     </div>
+    </Router>
     </ApolloProvider>
   );
 }
